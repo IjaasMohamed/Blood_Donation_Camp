5d03127f1e79cb578c05c043c109b872d5baf42d
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-01-18 17:01:52 -0300

    Delete Finalize directory

--- Finalize/js/new.js
+++
@@ -1,8 +0,0 @@
-function enableButton (){
-    if (document.getElementById("checkBox").checked){
-        document.getElementById("submitBtn").disabled=false;
-    }
-    else{
-        document.getElementById("submitBtn").disabled=true;
-    }
-}
